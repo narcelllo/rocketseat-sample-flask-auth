10423b937753bdb68ec3456411c9adbc00e09d65
Marcello Magno <marcello_magno@hotmail.com>
2024-07-29 16:11:54 -0300

    Added the login route and created validation for username and password.

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager, login_user,current_user
 
 
 app = Flask(__name__)
-app.config['SECURIT_KEY'] = "my_key"
+app.config['SECRET_KEY'] = "my_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+# view login
+login_manager.login_view = 'login'
+# Session <- conexão ativa
 
-@app.route("/initial", methods=(['GET']))
-def initial():
-    return "Initial OK"
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+ 
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Logado"})
+    
+    return jsonify({"message": "Credenciais invalidas"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
